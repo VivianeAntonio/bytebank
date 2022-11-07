f2aa2f7b55496c084dd60528ff843576a39e863d
VivianeAntonio <asilvaviviane@gmail.com>
2022-11-07 20:41:00 -0300

    Trabalhando com propriedades.

--- bytebank/Contas/ContaCorrente.cs
+++ bytebank/Contas/ContaCorrente.cs
@@ -1,16 +1,28 @@
-﻿using System;
+﻿using bytebank.Titular;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace bytebank
+namespace bytebank.Contas
 {
     public class ContaCorrente
     {
         public int numero_agencia;
-        public string conta;
-        public string titular;
+        public int Numero_agencia
+        {
+            get { return this.numero_agencia; }
+            set
+            {
+                if (value > 0)
+                {
+                    this.numero_agencia = value;
+                }
+            }
+        }
+        public string Conta { get; set; }
+        public Cliente Titular { get; set; }
         public double saldo = 100;
 
         public void Depositar(double valor)
@@ -47,10 +59,27 @@ namespace bytebank
 
         public void ExibirDadosDaConta()
         {
-            Console.WriteLine("Titular :" + titular);
-            Console.WriteLine("Conta :" + conta);
+            Console.WriteLine("Titular :" + Titular);
+            Console.WriteLine("Conta :" + Conta);
             Console.WriteLine("Número Agência :" + numero_agencia);
             Console.WriteLine("Saldo: " + saldo);
+        }
+
+        public void SetSaldo(double valor)
+        {
+            if (valor < 0)
+            {
+                return;
+            }
+            else
+            {
+                this.saldo = valor;
+            }
+        }
+
+        public double GetSaldo()
+        {
+            return this.saldo;
         }
 
     }
--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,21 +1,23 @@
 ﻿using bytebank;
+using bytebank.Contas;
+using bytebank.Titular;
 
-ContaCorrente contaDoAndre = new ContaCorrente();
-contaDoAndre.titular = "Andre Silva";
-contaDoAndre.numero_agencia = 15;
-contaDoAndre.conta = "1010-X";
-contaDoAndre.saldo = 100;
+//ContaCorrente contaDoAndre = new ContaCorrente();
+//contaDoAndre.titular = "Andre Silva";
+//contaDoAndre.numero_agencia = 15;
+//contaDoAndre.conta = "1010-X";
+//contaDoAndre.saldo = 100;
 
-ContaCorrente contaDoAndre2 = new ContaCorrente();
-contaDoAndre2.titular = "Andre Silva";
-contaDoAndre2.numero_agencia = 15;
-contaDoAndre2.conta = "1010-X";
-contaDoAndre2.saldo = 100;
+//ContaCorrente contaDoAndre2 = new ContaCorrente();
+//contaDoAndre2.titular = "Andre Silva";
+//contaDoAndre2.numero_agencia = 15;
+//contaDoAndre2.conta = "1010-X";
+//contaDoAndre2.saldo = 100;
 
 //contaDoAndre = contaDoAndre2;
 //Console.WriteLine(contaDoAndre==contaDoAndre2);
 
-contaDoAndre.ExibirDadosDaConta();
+//contaDoAndre.ExibirDadosDaConta();
 
 //Console.WriteLine("Saldo da conta do Andre: " + contaDoAndre.saldo);
 
@@ -69,3 +71,20 @@ contaDoAndre.ExibirDadosDaConta();
 //Console.WriteLine(valor2);
 
 //new ContaCorrente();
+
+ContaCorrente conta3 = new ContaCorrente();
+conta3.Numero_agencia = 18;
+conta3.Conta = "1011-H";
+conta3.SetSaldo(200);
+Console.WriteLine(conta3.GetSaldo());
+Console.WriteLine(conta3.Numero_agencia);
+Console.WriteLine(conta3.Conta);
+
+ContaCorrente conta4 = new ContaCorrente();
+conta4.Numero_agencia = 18;
+conta4.Conta = "1010-X";
+conta4.SetSaldo(500);
+conta4.Titular = new Cliente();
+Console.WriteLine(conta4.GetSaldo());
+Console.WriteLine(conta4.Numero_agencia);
+
